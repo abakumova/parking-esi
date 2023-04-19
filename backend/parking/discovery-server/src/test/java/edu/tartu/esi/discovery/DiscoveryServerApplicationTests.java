2ba3c9829e156dfa8be0780e7f66e2ef5f1f20b7
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2023-04-19 16:18:33 +0300

    Added parent.pom and discovery server

---
+++ backend/parking/discovery-server/src/test/java/edu/tartu/esi/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,12 @@
+package edu.tartu.esi.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}

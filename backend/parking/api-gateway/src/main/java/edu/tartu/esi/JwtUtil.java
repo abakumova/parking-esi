e72bf2561123e2b34888ed0b35596e8dba0aff6c
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2023-05-13 12:40:42 +0300

    BE - updated pom

---
+++ backend/parking/api-gateway/src/main/java/edu/tartu/esi/JwtUtil.java
@@ -0,0 +1,23 @@
+package edu.tartu.esi;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    public static final String SECRET = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}

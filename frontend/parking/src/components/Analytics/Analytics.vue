7d33950f210d1a046c15a8712cd0f3d5c005fd53
powermaxz <maxavramenko2018@gmail.com>
2023-05-14 17:17:30 +0300

    frontend - analytics logic

---
+++ frontend/parking/src/components/Analytics/Analytics.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="analytics">
+        <p>Occupancy: {{ occupancy }}</p>
+        <p>Revenue: {{ revenue }}</p>
+    </div>
+</template>
+
+<script>
+
+import AnalyticsService from "@/api/analytics/AnalyticsService";
+
+export default {
+    name: "Analytics",
+    props: {
+        slotId: {
+            type: String,
+            required: true,
+        }
+    },
+    data() {
+        return {
+            occupancy: null,
+            revenue: null
+        };
+    },
+    async created() {
+        const response = await AnalyticsService.getAnalyticsByParkingSlotId;
+        this.occupancy = response.data.occupancy;
+        this.revenue = response.data.revenue;
+    }
+};
+</script>
--- frontend/parking/src/components/ParkingManagement/ParkingCard/ParkingCard.vue
+++ frontend/parking/src/components/ParkingManagement/ParkingCard/ParkingCard.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="parking-card">
+    <div className="parking-card">
         <h2>{{ slot.name }}</h2>
-<!--        <p>Parking Id: {{ slot.id }}</p>-->
+        <!--        <p>Parking Id: {{ slot.id }}</p>-->
         <p>Location: {{ slot.location.formattedAddress }}</p>
         <p>Status: {{ slot.status }}</p>
         <p>Price: {{ slot.price }} €</p>
+        <Analytics :slotId="slot.id"/>
     </div>
 </template>
 
 <script>
 import './ParkingCard.css'
+import Analytics from "@/components/Analytics/Analytics.vue";
+
 export default {
-    name:"Parking Card",
+    name: "Parking Card",
+    components: {
+        Analytics
+    },
     props: {
         slot: {
             type: Object,
